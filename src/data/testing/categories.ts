1628bfe88dca955b806a549006715389fa5fa1f4
ngxCoder <ngxcoder@gmail.com>
2024-02-24 19:10:53 -0500

    feat: 🎸 some details

--- src/data/testing/categories.ts
+++ src/data/testing/categories.ts
@@ -1,20 +1,21 @@
-const Categories = [    "Clip del año",
-                        "Enfado del año",
-                        "Bitero del año",
-                        "Domada del año",
-                        "Dono del año",
-                        "Pareja del año",
-                        "Insulto del año",
-                        "Copy del año",
-                        "Gusano del año",
-                        //"In memoriam a los caídos del año y pomodoro",
-                        "Mod del año",
-                        "Fan made del año",
-                        "Evento del año",
-                        "Stream del año",
-                        "IRL del año",
-                        "Jugada del año",
-                        "Timing del año"
-                    ]
+const Categories = [
+  'Clip del año',
+  'Enfado del año',
+  'Bitero del año',
+  'Domada del año',
+  'Dono del año',
+  'Pareja del año',
+  'Insulto del año',
+  'Copy del año',
+  'Gusano del año',
+  //"In memoriam a los caídos del año y pomodoro",
+  'Mod del año',
+  'Fan made del año',
+  'Evento del año',
+  'Stream del año',
+  'IRL del año',
+  'Jugada del año',
+  'Timing del año'
+]
 
 export default Categories
