81f06d36f9235861cb177400e42197c43535c413
ngxCoder <ngxcoder@gmail.com>
2024-06-27 03:59:37 -0500

    ci: 🎡 add show sign out env variable and remove some warnings

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly TWITCH_CLIENT_SECRET: string
   readonly AUTH_SECRET: string
   readonly AUTH_TRUST_HOST: boolean
+  readonly SHOW_SIGN_OUT: boolean
 }
 
 interface ImportMeta {
