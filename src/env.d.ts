cabf6e20916f699f058f60ec1382f95aecf16e3e
ngxCoder <ngxcoder@gmail.com>
2024-07-06 04:42:39 -0500

    refactor: 💡 login flow using lucia

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,13 +2,21 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
+  readonly POSTGRES_URL: string
   readonly TWITCH_CLIENT_ID: string
   readonly TWITCH_CLIENT_SECRET: string
-  readonly AUTH_SECRET: string
-  readonly AUTH_TRUST_HOST: boolean
+  readonly TWITCH_REDIRECT_URI: string
   readonly SHOW_SIGN_OUT: boolean
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+/// <reference types="astro/client" />
+declare namespace App {
+  interface Locals {
+    session: import('lucia').Session | null
+    user: import('lucia').User | null
+  }
 }
---
+++ src/pages/login/twitch/index.ts
@@ -0,0 +1,21 @@
+import { generateState } from 'arctic'
+import { twitch } from '../../../lib/auth'
+
+import type { APIContext } from 'astro'
+
+export async function GET(context: APIContext): Promise<Response> {
+  const state = generateState()
+  const url = await twitch.createAuthorizationURL(state, {
+    scopes: ['user:read:email']
+  })
+
+  context.cookies.set('twitch_oauth_state', state, {
+    path: '/',
+    secure: import.meta.env.PROD,
+    httpOnly: true,
+    maxAge: 60 * 10,
+    sameSite: 'lax'
+  })
+
+  return context.redirect(url.toString())
+}
