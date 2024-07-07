fab5d20bc09807bae4c39cb83fea76cfef395366
ngxCoder <ngxcoder@gmail.com>
2024-07-07 15:26:07 -0500

    ci: 🎡 following vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
   security: {
     checkOrigin: true
