03e7e0e757559d0d96feefc2afec0838aa2de45c
ngxCoder <ngxcoder@gmail.com>
2024-07-30 19:12:45 -0500

    feat: 🎸 improve selector

--- src/components/CandidateSelector.tsx
+++ src/components/CandidateSelector.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import CandidateModal from './CandidateModal'
 import './CandidateSelector.css'
+import { icons as FeatherIcons } from 'feather-icons'
 
 interface CandidateSelectorProps {
   candidates?: Candidate[]
@@ -16,24 +17,74 @@ export default function CandidateSelector({
     open: false,
     candidate: undefined
   })
+  const [selectedCandidateId, setSelectedCandidateId] = useState<
+    number | undefined
+  >(undefined)
+
   return (
     <div className='flex flex-wrap justify-center gap-8'>
       {candidates?.map((candidate) => {
         const { thumbnail_url, title, id } = candidate
+        const isSelected = selectedCandidateId === id
 
         return (
           <div
             key={id}
-            className='candidate relative h-52 w-72 overflow-hidden bg-slate-600'
-            onClick={() => setIsCandidateVideoOpen({ open: true, candidate })}
+            className={`candidate relative h-52 w-72 overflow-hidden bg-slate-600 ${
+              isSelected && 'selected'
+            }`}
+            onClick={() => setSelectedCandidateId(id)}
           >
             <div
-              style={{ backgroundImage: `url(${thumbnail_url})` }}
+              style={{
+                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(87, 0, 15, 0.35)),url(${thumbnail_url})`
+              }}
               className='image left-0 top-0 h-full w-full bg-cover bg-no-repeat'
             />
-            <div className='overlay-pink absolute left-0 top-0 hidden h-full w-full cursor-pointer items-center justify-center bg-neutral-900 text-4xl text-white'>
-              <div className='flex h-full w-full items-center justify-center bg-black bg-opacity-60'>
+
+            <i
+              dangerouslySetInnerHTML={{
+                __html: FeatherIcons['check-circle'].toSvg({
+                  width: 36,
+                  height: 36,
+                  class: 'selected-icon'
+                })
+              }}
+              title='Seleccionado'
+            />
+
+            <div className='overlay-pink absolute left-0 top-0 hidden h-full w-full cursor-pointer items-center justify-center bg-neutral-900  text-white'>
+              <div className='flex h-full w-full flex-col items-center justify-center gap-4 bg-black bg-opacity-60 text-4xl'>
                 {title}
+
+                <div className='flex gap-4'>
+                  <i
+                    dangerouslySetInnerHTML={{
+                      __html: FeatherIcons.play.toSvg({
+                        width: 36,
+                        height: 36
+                      })
+                    }}
+                    title='Ver video'
+                    onClick={(e) => {
+                      setIsCandidateVideoOpen({ open: true, candidate })
+                      e.stopPropagation()
+                    }}
+                  />
+                  <i
+                    dangerouslySetInnerHTML={{
+                      __html: FeatherIcons['external-link'].toSvg({
+                        width: 36,
+                        height: 36
+                      })
+                    }}
+                    title='Abrir en una nueva pestaña'
+                    onClick={(e) => {
+                      window.open(candidate.url)
+                      e.stopPropagation()
+                    }}
+                  />
+                </div>
               </div>
             </div>
           </div>
